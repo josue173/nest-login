df348e19f2446582a2c0c44be2e20ba6cb0db57c
josue173 <josue.perez04@outlook.com>
2025-01-05 20:32:37 -0600

    feat(auth): Schema on MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { AuthModule } from './auth/auth.module';
+
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthService } from './auth.service';
+import { User, UserSchema } from './entities/auth.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
 })
 export class AuthModule {}
