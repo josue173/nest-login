6dad86c451d663168f8aecd8177534c24880bc7b
josue173 <josue.perez04@outlook.com>
2025-01-05 20:09:52 -0600

    feat: conexión con Mongo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Login en NestJS
+
+```
+docker compose up -d
+```
+Copiar el .env.template a .env
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost:27017')],
 })
 export class AppModule {}
