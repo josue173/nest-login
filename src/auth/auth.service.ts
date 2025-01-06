ff581e24eec75a37fc3c106a51f14403112d3fed
josue173 <josue.perez04@outlook.com>
2025-01-05 21:29:56 -0600

    feat(auth): create User

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { CreateUserDdo } from './dto/create-user.dto';
+import { Model } from 'mongoose';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/auth.entity';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDdo) {
-    return 'This action adds a new auth';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDdo): Promise<User> {
+    try {
+      const newUser = new this.userModel(createUserDto); // Creando instancia de un usuario
+      return await newUser.save();
+    } catch (error) {
+      if (error.code == 11000)
+        throw new BadRequestException(`${createUserDto.email} already exists`);
+      throw new InternalServerErrorException('Something wrong happened');
+    }
   }
 
   findAll() {
